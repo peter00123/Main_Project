0769e2a79de9f7dbd60cc471d109dd4ed5cf42a2
peter00123 <basilmathew833@gmail.com>
2026-02-19 10:35:41 +0530

    spring back end set up

---
+++ project/android/practice/app/src/main/java/ImageSliderAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.practice
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageSliderAdapter(private val images: List<Int>) :
+    RecyclerView.Adapter<ImageSliderAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val image: ImageView = view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.slide_img, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.image.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int = images.size
+}
---
+++ project/android/practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.practice
+
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+import android.widget.EditText
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val username = findViewById<EditText>(R.id.username)
+        val password = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.button)
+        val signup = findViewById<TextView>(R.id.signup)
+        val resetPassword = findViewById<TextView>(R.id.forgotPassword)
+
+
+
+        val correctUsername = "name"
+        val correctPassword = "1234"
+
+
+
+
+        fun showAlert(title: String, message: String) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(title)
+            builder.setMessage(message)
+            builder.setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.show()
+        }
+
+        signup.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+
+        }
+        resetPassword.setOnClickListener {
+            val intent = Intent(this, MainActivity4::class.java)
+            startActivity(intent)
+        }
+
+
+        loginButton.setOnClickListener {
+
+            val userInput = username.text.toString()
+            val passInput = password.text.toString()
+            /*val usernameStar = findViewById<TextView>(R.id.star)
+            val passwordStar = findViewById<TextView>(R.id.star)*/
+
+
+            if (userInput.isEmpty()) {
+               username.visibility = View.VISIBLE
+                username.error = "Username is required"
+                username.requestFocus()
+                return@setOnClickListener
+            }
+            if (passInput.isEmpty()) {
+                password.visibility = View.VISIBLE
+                password.error = "Password is required"
+                password.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+            if (/*userInput == correctUsername &&*/ passInput == correctPassword) {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("username", userInput)
+                Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+
+                startActivity(intent)
+
+
+
+
+            } else {
+
+                showAlert("Invalid Username or Password", "Please enter valid username and password")
+
+                /* Toast.makeText(this, "Invalid Username or Password", Toast.LENGTH_SHORT).show()*/
+
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
