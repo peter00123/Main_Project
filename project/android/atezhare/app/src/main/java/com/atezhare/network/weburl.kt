4cb6029483af334fb59c620867dbf249a6739646
peter <basilpetermathew@gmail.com>
2026-04-07 22:25:54 +0530

    published version 1.2

--- project/android/atezhare/app/src/main/java/com/atezhare/network/weburl.kt
+++ project/android/atezhare/app/src/main/java/com/atezhare/network/weburl.kt
@@ -1,4 +1,5 @@
 package com.atezhare.network
 
-//object WebURL {const val MAINURL: String = "https://main-project-cdol.onrender.com/atezhare/"}
-object WebURL {const val MAINURL: String = "http://192.168.10.34:8080/atezhare/"}
+object WebURL {const val MAINURL: String = "https://main-project-cdol.onrender.com/atezhare/"}
+//object WebURL {const val MAINURL: String = "http://192.168.10.40:8080/atezhare/"}
+
